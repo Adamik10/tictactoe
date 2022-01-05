a25a085eb913ea242c45ce634f0f99893462e307
Adam Antal <adoantal@gmail.com>
2022-01-05 13:17:57 +0100

    added square states and learned about react events

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,23 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import Square from "./Square";
 
 const Board = () => {
+  const [squareState, setSquareState] = useState(Array(9).fill(null));
+  const [isXnext, setIsXnext] = useState(true);
+
+  const handleSquareClick = (position) => {
+    setSquareState((prev) => {
+      return prev.map((square, pos) => {
+        if (square) {
+          return square;
+        }
+
+        if (pos === position) {
+          if (isXnext === true) {
+            setIsXnext(!isXnext);
+            return "X";
+          }
+          setIsXnext(!isXnext);
+          return "O";
+        }
+
+        return square;
+      });
+    });
+  };
+
+  const renderSquare = (position) => {
+    return (
+      <Square
+        value={squareState[position]}
+        onClick={() => {
+          handleSquareClick(position);
+        }}
+      />
+    );
+  };
+
   return (
     <div className="board">
       <div className="board-row">
-        <Square value={1} />
-        <Square value={2} />
-        <Square value={3} />
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
       <div className="board-row">
-        <Square value={4} />
-        <Square value={5} />
-        <Square value={6} />
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
       <div className="board-row">
-        <Square value={7} />
-        <Square value={8} />
-        <Square value={9} />
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
