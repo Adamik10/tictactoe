f47772411a1820bae0270417e6cb2f14a62efe65
Adam Antal <adoantal@gmail.com>
2022-01-07 15:37:45 +0100

    -

--- todo.jsx
+++ todo.jsx
@@ -5,5 +5,5 @@
 // after winner declared, stop the game //DONE
 
 // add game history
-// refactor code to keep track of an array of objects with current board and the current player
-// fix the rest of the code accordingly
+// refactor code to keep track of an array of objects with current board and the current player // DONE
+// fix the rest of the code accordingly // DONE
